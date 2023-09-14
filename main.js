cc489f31298cad36fb6fa9fb9f295760b0fc13a5
eliangonzalez84 <anunciatres.m@gmail.com>
2023-09-14 01:40:46 -0300

    agrego detalles de vuelos populares

--- main.js
+++ main.js
@@ -159,89 +159,89 @@ close.addEventListener('click', () => {
   modal_container.classList.remove('show');
 })
 
-//2
-// const open2 = document.getElementById('open2');
-// const modal_container2 = document.getElementById('modal_container2');
-// const close2 = document.getElementById('close2');
-// open2.addEventListener('click', () => {
-//     modal_container2.classList.add('show');  
+2
+const open2 = document.getElementById('open2');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('close2');
+open2.addEventListener('click', () => {
+    modal_container2.classList.add('show');  
   
-//   });
-//   close2.addEventListener('click', () => {
-//     modal_container2.classList.remove('show');
-//   })
+   });
+  close2.addEventListener('click', () => {
+    modal_container2.classList.remove('show');
+  })
 
-// //3
-// const open3 = document.getElementById('open3');
-// const modal_container3 = document.getElementById('modal_container3');
-// const close3 = document.getElementById('close3');
-// open3.addEventListener('click', () => {
-//     modal_container3.classList.add('show');  
+//3
+const open3 = document.getElementById('open3');
+const modal_container3 = document.getElementById('modal_container3');
+const close3 = document.getElementById('close3');
+open3.addEventListener('click', () => {
+    modal_container3.classList.add('show');  
   
-//   });
-//   close3.addEventListener('click', () => {
-//     modal_container3.classList.remove('show');
-//   })
+  });
+  close3.addEventListener('click', () => {
+    modal_container3.classList.remove('show');
+  })
 
-//   //4
-// const open4 = document.getElementById('open4');
-// const modal_container4 = document.getElementById('modal_container4');
-// const close4 = document.getElementById('close4');
-// open4.addEventListener('click', () => {
-//     modal_container4.classList.add('show');  
+  //4
+const open4 = document.getElementById('open4');
+const modal_container4 = document.getElementById('modal_container4');
+const close4 = document.getElementById('close4');
+open4.addEventListener('click', () => {
+    modal_container4.classList.add('show');  
   
-//   });
-//   close4.addEventListener('click', () => {
-//     modal_container4.classList.remove('show');
-//   })
+  });
+  close4.addEventListener('click', () => {
+    modal_container4.classList.remove('show');
+  })
 
-// //5
-// const open5 = document.getElementById('open5');
-// const modal_container5 = document.getElementById('modal_container5');
-// const close5 = document.getElementById('close5');
-// open5.addEventListener('click', () => {
-//     modal_container5.classList.add('show');  
+//5
+const open5 = document.getElementById('open5');
+const modal_container5 = document.getElementById('modal_container5');
+const close5 = document.getElementById('close5');
+open5.addEventListener('click', () => {
+    modal_container5.classList.add('show');  
   
-//   });
-//   close5.addEventListener('click', () => {
-//     modal_container5.classList.remove('show');
-//   })
+  });
+  close5.addEventListener('click', () => {
+    modal_container5.classList.remove('show');
+  })
 
-// //6
-// const open6 = document.getElementById('open6');
-// const modal_container6 = document.getElementById('modal_container6');
-// const close6 = document.getElementById('close6');
-// open6.addEventListener('click', () => {
-//     modal_container6.classList.add('show');  
+//6
+const open6 = document.getElementById('open6');
+const modal_container6 = document.getElementById('modal_container6');
+const close6 = document.getElementById('close6');
+open6.addEventListener('click', () => {
+    modal_container6.classList.add('show');  
   
-//   });
-//   close6.addEventListener('click', () => {
-//     modal_container6.classList.remove('show');
-//   })
+  });
+  close6.addEventListener('click', () => {
+    modal_container6.classList.remove('show');
+  })
   
-// //7
-// const open7 = document.getElementById('open7');
-// const modal_container7 = document.getElementById('modal_container7');
-// const close7 = document.getElementById('close7');
-// open7.addEventListener('click', () => {
-//     modal_container7.classList.add('show');  
+//7
+const open7 = document.getElementById('open7');
+const modal_container7 = document.getElementById('modal_container7');
+const close7 = document.getElementById('close7');
+open7.addEventListener('click', () => {
+    modal_container7.classList.add('show');  
   
-//   });
-//   close7.addEventListener('click', () => {
-//     modal_container7.classList.remove('show');
-//   })
+  });
+  close7.addEventListener('click', () => {
+    modal_container7.classList.remove('show');
+  })
   
-// //8
-// const open8 = document.getElementById('open8');
-// const modal_container8 = document.getElementById('modal_container8');
-// const close8 = document.getElementById('close8');
-// open8.addEventListener('click', () => {
-//     modal_container8.classList.add('show');  
+//8
+const open8 = document.getElementById('open8');
+const modal_container8 = document.getElementById('modal_container8');
+const close8 = document.getElementById('close8');
+open8.addEventListener('click', () => {
+    modal_container8.classList.add('show');  
   
-//   });
-//   close8.addEventListener('click', () => {
-//     modal_container8.classList.remove('show');
-//   })
+  });
+  close8.addEventListener('click', () => {
+    modal_container8.classList.remove('show');
+  })
   
 // //9
 // const open9 = document.getElementById('open9');
